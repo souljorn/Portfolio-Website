931331cfb1ef823b583b92035da67246acff4e08
Tim Botelho <souljorn@gmail.com>
2018-08-21 13:00:27 -0400

    Fixed Splash and created hover overs for icons in the footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -2,6 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom";
 import styled from 'styled-components';
 import logo from '../images/Merkaba.svg';
+import appCss from '../../src/app.css'
 import { library } from '@fortawesome/fontawesome-svg-core'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faCoffee } from '@fortawesome/free-solid-svg-icons'
@@ -14,7 +15,7 @@ display: block;
 align-items: center;
 background-color:#DEDBDB;
 height: 100px;
-max-width: 1000px;
+
 margin-left: auto;
 margin-right: auto;
 @media all and (max-width: 600px){
@@ -22,6 +23,15 @@ margin-right: auto;
   float:bottom;
 }
 `;
+
+const icon = {
+  padding:'20px',
+  color:'#333',
+  // radium provides hover states and vendor prefixes
+  ':hover': {
+    transform: 'rotateY(180deg) scale(1.5)'
+  },
+};
 
 class Footer extends React.Component {
   constructor(props){
@@ -33,21 +43,24 @@ class Footer extends React.Component {
       <Container>
         <a  href='https://github.com/souljorn'>
           <FontAwesomeIcon
+            className="icons"
             icon={['fab', 'github']}
             size="2x"
             style={ {padding:'20px', color:'#333'}}/>
         </a>
         <a href='https://www.instagram.com/souljorn/'>
           <FontAwesomeIcon
+            className="icons"
             icon={['fab', 'instagram']}
             size="2x"
             style={ {padding:'20px', color:'#333'}}/>
         </a>
         <a  href='https://www.linkedin.com/in/timbotelho/'>
           <FontAwesomeIcon
+            className="icons"
             icon={['fab', 'linkedin-in']}
             size="2x"
-            style={ {padding:'20px', color:'#333'}}/>
+            style={icon}/>
         </a>
       </Container>
     );
--- src/index.js
+++ src/index.js
@@ -2,7 +2,8 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import PhotoGrid from "./components/PhotoGrid";
-import Header from "./components/Header";
+import  Header from "./components/Header";
+import Home from "./components/Home";
 import Footer from "./components/Footer";
 import styled from 'styled-components';
 import './components/fonts.js'
@@ -16,53 +17,6 @@ margin-right: auto;
 @media all and (max-width: 600px){
   display: block;
 }
-`;
-
-const SplashContainer = styled.div`
-position: relative;
-margin-left: auto;
-margin-right: auto;
-padding: 0px;
-width:1000px;
-@media all and (max-width: 600px){
-  width: 100%;
-
-}
- `;
-
-const Splash = styled.img`
-text-align:center;
-margin-left: auto;
-margin-right: auto;
-@media all and (max-width: 600px){
-  width: 100%;
-
-}
-`;
-
-const SplashOverlay = styled.div`
-  position: absolute;
-  background: rgb(0, 0, 0);
-  background: rgba(0, 0, 0, 0.5); /* Black see-through */
-  color: #f1f1f1;
-  width: 500px;
-  opacity:100;
-  color: white;
-  font-size: 18px;
-  font-family:'Roboto';
-  font-weight: 100;
-  padding: 20px;
-  text-align: center;
-  max-width: 1000px;
-  margin-left: auto;
-  margin-right: auto;
-  bottom:300px;
-  right: 200px;
-  @media all and (max-width: 600px){
-      bottom: 70px
-      width: 150px;
-      right: 80px;
-  }
 `;
 
 class Index extends React.Component {
@@ -86,10 +40,7 @@ class Index extends React.Component {
     return (
        <PhotoContainer>
          <Header/>
-         <SplashContainer>
-           <Splash src={splash} width= '1000px'></Splash>
-           <SplashOverlay>Softare Developer and Webdesigner</SplashOverlay>
-         </SplashContainer>
+         <Home/>
          <Footer/>
       </PhotoContainer>
     );
